785b20c5425726494c4a4318084aaa58324644d1
lekealex <106382948+lekealex@users.noreply.github.com>
2023-03-03 16:55:24 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script> 
+
+export default{
+  data(){
+    return{
+      contador:10
+    }
+  },
+  methods: {
+    incrementar(){
+    this.contador++
+  },
+  decrementar(){
+    if (this.contador > 0) {
+        this.contador--
+      }
+    },
+    resetar(){
+      this.contador = 0
+    }
+  }
+}
+
+</script> 
+
+
+
+<template> 
+
+  <button @click="incrementar">+</button> 
+  <button @click="decrementar">-</button>
+  <button @click="resetar">C</button> 
+  <p>Valor do contador: {{ contador }}</p>
+  
+
+</template>
+
+
+
+<style scoped> 
+
+button {
+  margin-left: 10px;
+  background-color: black;
+  color: white;
+  border:none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+</style>
